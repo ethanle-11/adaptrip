24294988394e1e80b1d049aaf08543a59946f0fa
Ethan Le <ethanle@unc.edu>
2026-07-03 03:47:20 -0400

    finished create trip page UI and supabase connection

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import CreateTrip from './pages/CreateTrip';
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/create" element={<CreateTrip />} />
     </Routes>
   </BrowserRouter>
   )
