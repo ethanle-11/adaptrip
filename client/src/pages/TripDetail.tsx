6ac28b20fa2cfc38960b82de2ac5b3dc4568fea6
Ethan Le <ethanle@unc.edu>
2026-07-04 00:23:22 -0400

    fixed trip detail and added map imports

--- client/src/pages/TripDetail.tsx
+++ client/src/pages/TripDetail.tsx
@@ -1,5 +1,17 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import { supabase } from '../lib/supabase'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
+
+L.Icon.Default.mergeOptions({
+    iconUrl: markerIcon,
+    shadowUrl: markerShadow
+})
+
 
 function TripDetail() {
     const { id } = useParams()
@@ -8,6 +20,26 @@ function TripDetail() {
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState('')
 
+    useEffect(() => {
+
+        const fetchTripData = async () => {
+            try {
+                const { data, error } = await supabase.from('trips').select('*').eq('id', id).single()
+                if (error) throw error
+                setTrip(data)
+
+                const { data: activitiesData, error: activitiesError} = await supabase.from('activities').select('*').eq('trip_id', id)
+                if (activitiesError) throw activitiesError
+                setActivities(activitiesData)
+            } catch (error) {
+                setError("Something went wrong")
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchTripData()
+     
+    }, [])
 }
 
 export default TripDetail
