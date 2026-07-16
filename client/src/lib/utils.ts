fc43422179b982d900016b4eeb2ef27f95e8875c
Ethan Le <ethanle@unc.edu>
2026-07-16 04:17:37 -0400

    finished adaptation engine

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,5 +1,5 @@
 
-{/* Format Date Function */}
+// Format Date Function
 
 export const formatDate = (dateString: string) => {
     const date = new Date(dateString)
@@ -7,7 +7,7 @@ export const formatDate = (dateString: string) => {
 }
 
 
-{/* Stores all dates between start + end dates of a trip */}
+// Stores all dates between start + end dates of a trip
 
 export const getDaysBetween = (start: string, end: string): Date[] => {
     const days: Date[] = []
@@ -20,4 +20,18 @@ export const getDaysBetween = (start: string, end: string): Date[] => {
     }
 
     return days
+}
+
+// Get distance between two places using Haversine formula
+
+export const getDistanceBetween = (lat1: number, lon1: number, lat2: number, lon2: number) => {
+    const R = 6371
+    const dLat = (lat2 - lat1) * Math.PI / 180
+    const dLon = (lon2 - lon1) * Math.PI / 180
+    const a = 
+        Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+        Math.sin(dLon/2) * Math.sin(dLon/2)
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+    return R * c
 }
